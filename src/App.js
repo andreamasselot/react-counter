dbc8bb99012733a69a8a8fa0f161c1ac4d992b3f
Andréa Masselot <andrea.nicolas128@gmail.com>
2022-11-02 14:42:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+        <h1> <i class="fa-solid fa-calculator"></i> React Counter</h1>
       </header>
+      <div className='container'>
+        <div className='counter-container'>
+
+          {counter > 0 ?
+            <button className='count' onClick={() => {
+              setCounter(counter - 1);
+            }}>-</button> : <div className='count'></div>}
+
+          <p className='numbers'>{counter}</p>
+
+          {counter < 10 ?
+            <button className='count' onClick={() => {
+              setCounter(counter + 1);
+            }}>+</button> : <div className='count'></div>}
+
+
+        </div>
+        <button className='reset-button' onClick={() => {
+          setCounter(0)
+        }} >Reset</button>
+      </div>
+      <footer>
+        <p>Made with <span>React</span> at <a href='https://www.lereacteur.io/'>Le Reacteur</a> by <a href='https://github.com/andreamasselot'>Andréa</a></p>
+      </footer>
+
     </div>
   );
 }
